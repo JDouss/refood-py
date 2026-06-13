8314bccf8f9d5fe438762fc619d22a19454b97cd
jortegamoreno <joaquin.om5@gmail.com>
2026-06-13 12:17:46 +0200

    feat: Creamos nueva pagina para estadisticas

--- refood_app/views.py
+++ refood_app/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from django.db.models import Sum
+from datetime import datetime, timedelta
 
 from .forms import MenuPrincipalForm,EntradasForm,SalidasForm
 from .models import AlEnt,Donante,TipoAl,AlSal,Benef
@@ -71,3 +73,35 @@ def salidas(request):
     return render(request, 'refood_app/salidas.html', {
             'form': form,
         })
+
+@login_required
+def estadisticas(request):
+    # Lógica para obtener los datos de los gráficos
+    # Kilos totales por día (últimos 7 días)
+    today = datetime.now().date()
+    dates = [today - timedelta(days=i) for i in range(7)]
+    dates.reverse() # Para tener las fechas en orden ascendente
+
+    kilos_por_dia = []
+    for d in dates:
+        total_kilos = AlEnt.objects.filter(fecha_y_hora_entrada__date=d).aggregate(Sum('peso'))['peso__sum'] or 0
+        kilos_por_dia.append(float(total_kilos)) # Convertir a float para Chart.js
+
+    # Kilos medios por día (últimos 7 días)
+    kilos_medios_por_dia = []
+    for d in dates:
+        total_kilos_salida = AlSal.objects.filter(fecha_salida__date=d).aggregate(Sum('al_ent__peso'))['al_ent__peso__sum'] or 0
+        num_salidas = AlSal.objects.filter(fecha_salida__date=d).count()
+        
+        if num_salidas > 0:
+            media_kilos = total_kilos_salida / num_salidas
+        else:
+            media_kilos = 0
+        kilos_medios_por_dia.append(float(media_kilos))
+
+    context = {
+        'dates': [d.strftime('%Y-%m-%d') for d in dates],
+        'kilos_por_dia': kilos_por_dia,
+        'kilos_medios_por_dia': kilos_medios_por_dia,
+    }
+    return render(request, 'refood_app/estadisticas.html', context)
